1568bfbed22c95ca891c87e1f7aaec471f6f3b93
aran <aas711@nyu.edu>
2015-09-22 11:03:07 -0800

    hue

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	
+	public Transform player;
+	public Text textUI;
+	public Transform purp;
+	public Transform orange;
+	public Transform green;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textUI.text = "";
+		if ( (Vector3.Distance(new Vector3(23.7f, 0f, -34.3f), player.position) < 10f) ){
+			textUI.text = "99999 the purple one knows 9999";
+		}
+		if ( (Vector3.Distance(purp.position, player.position) < 10f) ){
+			textUI.text = "121212 yes, but i'm rather busy right now, i'm afraid 121212" + 
+				"\n you might have better luck asking the green one";
+		}
+		if ( (Vector3.Distance(green.position, player.position) < 10f) ){
+			textUI.text = "3333 da, da 3333" + "\n your apotheosis is close" + "\n and your spacebar is the key";
+		}
+		if ( (Vector3.Distance(orange.position, player.position) < 10f) && Input.GetKeyDown(KeyCode.Space)){
+			textUI.text = "YOU WIN";
+		}
+	}
+}
---
+++ Assets/cont.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class cont : MonoBehaviour {
+	public float moveSpeed = 38f;
+	public float turnSpeed = 90f;
+	
+	
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W) ) {
+			transform.position += new Vector3(0f,0f,moveSpeed * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0f,0f,0f); 
+		}
+		
+		else if (Input.GetKey (KeyCode.A) ) {
+			transform.position += new Vector3(-moveSpeed* Time.deltaTime,0f,0f);
+			transform.eulerAngles = new Vector3(0f,270f,0f); 
+}
+		else if (Input.GetKey (KeyCode.S) ) {
+			transform.position += new Vector3(0f,0f,-moveSpeed* Time.deltaTime);
+			transform.eulerAngles = new Vector3(0f,180f,0f);
+		}
+		else if (Input.GetKey (KeyCode.D) ) {
+			transform.position += new Vector3(moveSpeed * Time.deltaTime,0f,0f);
+			transform.eulerAngles = new Vector3(0f,90f,0f) ;
+			}
+		
+		Camera.main.transform.position = transform.position + new Vector3(0f,29f,-15f);
+	}
+}
